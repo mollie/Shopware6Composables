61a637e34f9cb925a9835459b2b4bcb8a2805fdb
Niklas Wolf <wolfniklas94@web.de>
2024-06-21 11:46:16 +0200

    fix linter errors

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -6,54 +6,56 @@ const CreditCardToken = ref()
 const IdealError = ref()
 </script>
 <template>
-    <h1>Nuxt mollie payments components</h1>
+    <div>
+        <h1>Nuxt mollie payments components</h1>
 
-    <h2>Credit Card</h2>
+        <h2>Credit Card</h2>
 
-    <ShopwareFrontendsCreditCard
-        submit-button-label="Save"
-        locale="en_US"
-        :submit-disabled="!!CreditCardToken"
-        @submit="
-            (token) => {
-                CreditCardToken = `${token} ✔️`
-                CreditCardError = null
-            }
-        "
-        @error="
-            (message) => {
-                CreditCardError = `${message} ❌`
-            }
-        "
-    />
+        <ShopwareFrontendsCreditCard
+            submit-button-label="Save"
+            locale="en_US"
+            :submit-disabled="!!CreditCardToken"
+            @submit="
+                (token) => {
+                    CreditCardToken = `${token} ✔️`
+                    CreditCardError = null
+                }
+            "
+            @error="
+                (message) => {
+                    CreditCardError = `${message} ❌`
+                }
+            "
+        />
 
-    <div class="demo-mollie-results">
-        <div>
-            Test Credit Number:
-            <pre>2223 0000 1047 9399</pre>
+        <div class="demo-mollie-results">
+            <div>
+                Test Credit Number:
+                <pre>2223 0000 1047 9399</pre>
+            </div>
+            <hr />
+            <div v-if="CreditCardError">Error: {{ CreditCardError }}</div>
+            <div v-if="CreditCardToken">Token: {{ CreditCardToken }}</div>
         </div>
-        <hr />
-        <div v-if="CreditCardError">Error: {{ CreditCardError }}</div>
-        <div v-if="CreditCardToken">Token: {{ CreditCardToken }}</div>
+
+        <h2>Ideal</h2>
+
+        <ShopwareFrontendsIdeal
+            locale="en_US"
+            select-label="Please choose your bank:"
+            select-disabled-option="Select your bank"
+            @save-issuer-success="
+                () => {
+                    IdealError = null
+                }
+            "
+            @save-issuer-error="
+                (message) => {
+                    IdealError = `${message} ❌`
+                }
+            "
+        />
     </div>
-
-    <h2>Ideal</h2>
-
-    <ShopwareFrontendsIdeal
-        locale="en_US"
-        select-label="Please choose your bank:"
-        select-disabled-option="Select your bank"
-        @save-issuer-success="
-            () => {
-                IdealError = null
-            }
-        "
-        @save-issuer-error="
-            (message) => {
-                IdealError = `${message} ❌`
-            }
-        "
-    />
 </template>
 <style scoped>
 .demo-mollie-results {
